03cd156efdc894e05d26ff5aa927e6d14f23ca82
박신영 <psy_chopath@naver.com>
2025-05-05 00:01:03 +0900

    레벨1 일부 수정

--- src/com/example/calculator/level1/CalculatorLV1.java
+++ src/com/example/calculator/level1/CalculatorLV1.java
@@ -1,44 +1,38 @@
 package com.example.calculator.level1;
 
-import java.io.*;
+import java.util.Scanner;
 
 public class CalculatorLV1 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String input;
 
-        while(!(input = br.readLine()).equals("exit")) {
+        while (!(input = scanner.next()).equals("exit")) {
             int a = input.charAt(0) - '0';
             char operator = input.charAt(1);
             int b = input.charAt(2) - '0';
             int result = 0;
 
             switch (operator) {
-                case '+':
-                    result = a + b;
-                    break;
-                case '-':
-                    result = a - b;
-                    break;
-                case '*':
-                    result = a * b;
-                    break;
-                case '/':
+                case '+' -> result = a + b;
+                case '-' -> result = a - b;
+                case '*' -> result = a * b;
+                case '/' -> {
                     if (b == 0) {
                         result = -1;
                         break;
                     }
                     result = a / b;
-                    break;
+                }
             }
 
             if (result == -1) {
                 System.out.println("0으로 나눌 수는 없습니다.");
-            }
-            else{
+            } else {
                 System.out.println(result);
             }
         }
 
+        scanner.close();
     }
 }
